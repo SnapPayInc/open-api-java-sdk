5e35376f737699330dc1bade6a3a5c0f21879031
Shawn Du <shawn.du@snappay.ca>
2019-11-14 14:57:22 -0500

    refine the signature utilities

--- src/main/java/ca/snappay/openapi/sign/MD5.java
+++ src/main/java/ca/snappay/openapi/sign/MD5.java
@@ -1,35 +1,30 @@
-package com.wiseasy.openapi.sign;
+package ca.snappay.openapi.sign;
 
 import org.apache.commons.codec.digest.DigestUtils;
+
 import java.io.UnsupportedEncodingException;
-import java.security.SignatureException;
 
 /**
- * @Auther: liqie
- * @Date: 2018/11/2 09:42
- * @Description:
+ * Utility class for MD5 signature.
+ *
+ * @author shawndu
+ * @version 1.0
  */
 public class MD5 {
 
     /**
-     * 签名字符串
-     * @param text 需要签名的字符串
-     * @param key 密钥
-     * @param input_charset 编码格式
-     * @return 签名结果
+     * Signs a string.
+     *
+     * @param text    the string to be signed.
+     * @param key     the private key.
+     * @param charset the character set.
+     * @return the signature.
      */
-    public static String md5sign(String text, String key, String input_charset) {
+    public static String md5sign(String text, String key, String charset) {
         text = text + key;
-        return DigestUtils.md5Hex(getContentBytes(text, input_charset));
+        return DigestUtils.md5Hex(getContentBytes(text, charset));
     }
 
-    /**
-     * @param content
-     * @param charset
-     * @return
-     * @throws SignatureException
-     * @throws UnsupportedEncodingException
-     */
     private static byte[] getContentBytes(String content, String charset) {
         if (charset == null || "".equals(charset)) {
             return content.getBytes();
@@ -37,7 +32,7 @@ public class MD5 {
         try {
             return content.getBytes(charset);
         } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("MD5签名过程中出现错误,指定的编码集不对,您目前指定的编码集是:" + charset);
+            throw new RuntimeException("Unsupported character set: " + charset);
         }
     }
 }
